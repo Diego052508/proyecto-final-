28c86787d82e07667fb5baffbebb6d42934e0fae
KendraARS <keareyes@uamv.edu.ni>
2025-07-04 07:17:54 -0600

    Update menu.py

--- menu.py
+++ menu.py
@@ -60,17 +60,6 @@ def guardar_datos():
             nombre = clientes.get(a["cedula"], {}).get("nombre", "Desconocido")
             f.write(f"Cédula: {a['cedula']} | Nombre: {nombre} | Fecha: {a['fecha'].date()}\n")
 
-def validar_entrada(nombre, cedula, contacto, tipo_membresia):
-    if not nombre.replace(" ", "").isalpha():
-        return False, "El nombre solo debe contener letras."
-    if not (1 <= len(cedula) <= 5 and cedula.isalnum()):
-        return False, "La cédula debe contener hasta 5 caracteres alfanuméricos."
-    if not (contacto.isdigit() and len(contacto) == 8):
-        return False, "El contacto debe contener 8 dígitos numéricos."
-    if tipo_membresia.lower() not in ["mensual", "diaria"]:
-        return False, "El tipo de membresía debe ser 'mensual' o 'diaria'."
-    return True, ""
-
 
 def registrar_pago():
     cedula = input("Cédula del cliente: ")
